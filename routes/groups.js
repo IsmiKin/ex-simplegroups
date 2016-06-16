79ccde6e995ec535c330e7af1fea2e61c6bb14fe
Ismael Ors <ismikin@gmail.com>
2016-06-16 11:44:39 +0200

    Add one group route

--- routes/groups.js
+++ routes/groups.js
@@ -6,16 +6,31 @@ var unirest = require('unirest');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     var prefix = req.query.prefix || '';
-    Request = unirest.get(process.env.kjuesEndpoint + '/api/1/groups?prefix=' + req.query.prefix)
+    Request = unirest.get(process.env.endpoint + '/api/1/groups?prefix=' + req.query.prefix)
     .headers({'Accept': 'application/json', 'Content-Type': 'application/json'});
 
     Request.auth({
-      user: process.env.kjuesUsername,
-      pass: process.env.kjuesPassword,
+      user: process.env.apiUsername,
+      pass: process.env.apiPassword,
       sendImmediately: true
     });
     Request.end(function(response){
-      console.log("returning smth");
+      res.status(200).send(response.body);
+    });
+});
+
+router.get('/:groupid', function(req, res, next) {
+    var groupid = req.params.groupid || '';
+
+    Request = unirest.get(process.env.endpoint + '/api/1/groups/' + groupid)
+    .headers({'Accept': 'application/json', 'Content-Type': 'application/json'});
+
+    Request.auth({
+      user: process.env.apiUsername,
+      pass: process.env.apiPassword,
+      sendImmediately: true
+    });
+    Request.end(function(response){
       res.status(200).send(response.body);
     });
 });
